aa578eda1a0dc808afe39a4bed81f98736697ddf
songzhentian <songzhentian1993@gmail.com>
2019-02-22 15:21:04 +0800

    test another

--- comments/testcomment.pl
+++ comments/testcomment.pl
@@ -1,8 +1,22 @@
 use IO::CaptureOutput qw/capture_exec/;
 
-@command = ("comments", "-c1", "main.cpp");
+@command1 = ("comments", "-c1", "main.cpp");
 
+#($stdout, $error, $success, $status) = capture_exec( @command );
+#print "command1--------";
+#print "$stdout\n";
+#print "$error\n";
+#print "$success\n";
+#print "$status\n";
+
+@command2 = ("head", "-400", "main.cpp");
 ($stdout, $error, $success, $status) = capture_exec( @command );
+print "command2--------";
+print "$stdout\n";
+print "$error\n";
+print "$success\n";
+print "$status\n";
+
 
 $output_file = "main.cpp.testcomments";
 open $output_fh, '>', $output_file or die "can't create output file [$output_file]: $!";
